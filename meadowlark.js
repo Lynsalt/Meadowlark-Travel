1927816ae295c74400421521c8ffcaa029b4d396
Lynden Saltibus <menelike3@gmail.com>
2021-02-12 21:27:36 -0500

    First initial commit

---
+++ meadowlark.js
@@ -0,0 +1,46 @@
+const express = require('express');
+
+const app = express();
+
+//set up handlebars view engine
+var handlebars = require('express3-handlebars')
+ .create({ defaultLayout:'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/public'));
+
+var fortunes =[
+    "Conquer your fears or they will conquer you.",
+    "Rivers need springs.",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simple.", 
+];
+
+
+app.get('/', (req,res)=>{
+    res.render('home');
+});
+
+app.get('/about', (req,res)=>{
+    let randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+    res.render('about',{fortune: randomFortune})
+})
+
+//custom 404 page
+app.use((req,res)=>{
+    res.status(404);
+    res.render('404');
+});
+
+//custom 500 page
+app.use((req,res)=>{
+    res.status(500);
+    res.render('500');
+});
+
+app.listen(app.get('port'));
